633156aac21b328f13f84f79c8a098c768138f34
marcosscpp <marcosscpp@gmail.com>
2024-02-07 00:42:43 -0300

    Inserindo parte do grid de infos

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 function heightAdjuster() {
   const introContent = document.querySelectorAll("[data-content]");
   const orangeShape = document.querySelectorAll("[data-bg]");
-
   orangeShape.forEach((element, index) => {
     introContent[index].style.height = element.offsetHeight + 10 + "px";
   });
@@ -9,3 +8,19 @@ function heightAdjuster() {
 
 heightAdjuster();
 window.addEventListener("resize", heightAdjuster);
+
+const swiper = new Swiper(".info-cards__swipper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
