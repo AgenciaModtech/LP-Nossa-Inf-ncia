ef90376438bdc0d94dd1dd2fdbb8383089d8f7f6
marcosscpp <psrmarcoss@gmail.com>
2024-02-07 23:55:14 -0300

    Inserindo section de professores

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,16 @@
 const swiper = new Swiper(".info-cards__swipper", {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
